2ece42a4d191498407af3bc141645b345c7650ee
Niket1-1 <82999578+Niket1-1@users.noreply.github.com>
2021-08-27 17:04:05 +0530

    niket all files

---
+++ script.js
@@ -0,0 +1,25 @@
+
+    (function(w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+            "gtm.start": new Date().getTime(),
+            event: "gtm.js"
+        });
+        var f = d.getElementsByTagName(s)[0],
+            j = d.createElement(s),
+            dl = l != "dataLayer" ? "&l=" + l : "";
+        j.async = true;
+        j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, "script", "dataLayer", "GTM-NF375C");
+
+
+
+    window._bs = window._bs || {};
+
+function login(){
+    window.location.href="login.html"
+}
+function signup(){
+    window.location.href="joinnow.html"
+}
